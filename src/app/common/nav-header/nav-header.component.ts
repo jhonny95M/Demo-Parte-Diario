ccfb136d2c794990130112eb10739727b72e1ccf
jhonny95M <juniorjhonnymalpartida@gmail.com>
2023-05-27 17:16:07 -0500

    los cambios similar a ememsa produccion

--- src/app/common/nav-header/nav-header.component.ts
+++ src/app/common/nav-header/nav-header.component.ts
@@ -10,4 +10,12 @@ export class NavHeaderComponent {
   TimeElapsed!:string;
   TipoCambio!:Number;
   AxuAuthenticateState!:AxuAuthenticateState;
+  constructor(){
+    let state=new AxuAuthenticateState();
+    state.apellidoParterno="Malpartida";
+    state.apellidoMaterno="Roncal";
+    state.nombres="Junior";
+    state.perfil="Analista Programador";
+    this.AxuAuthenticateState=state;
+  }
 }
